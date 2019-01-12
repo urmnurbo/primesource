6016b60182ed0d42990b136253527a78274560af
User <User@192.168.8.103>
2019-01-12 20:30:04 +0600

    re1

--- restapi/src/main/java/com/primesource/restapi/repository/UserRepository.java
+++ restapi/src/main/java/com/primesource/restapi/repository/UserRepository.java
@@ -3,14 +3,10 @@ package com.primesource.restapi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.scheduling.annotation.Async;
+
 
 import com.primesource.restapi.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long >{
-	/*@Async
-    @Query("SELECT * FROM users u where u.username like '%:username%'") */
     List<User> findByUsernameContainingIgnoreCase(String username);
 }
